414986ecc8ec38cc12480aed2a44e1251b5539f0
Peter L-G <peter.larsson.green@gmail.com>
2015-01-05 10:38:34 +0100

    Made it a meteor package.

---
+++ package.js
@@ -0,0 +1,11 @@
+Package.describe({
+	name: 'peppelg:drawjs',
+	summary: 'Animate drawings with a pen on canvases.',
+	version: '1.0.0',
+	name: "peppelg:drawjs",
+	git: 'https://github.com/PeppeL-G/drawjs.git'
+})
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.0.2.1')
+})
